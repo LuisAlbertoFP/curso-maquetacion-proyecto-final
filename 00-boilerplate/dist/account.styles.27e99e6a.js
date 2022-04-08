1bfe73d157bf7989f1fca411a56c0ee5598305a2
Luis Alberto <luisalb@edu.xunta.es>
2022-04-08 16:51:34 +0200

    pre trabajo

--- 00-boilerplate/dist/account.styles.27e99e6a.js
+++ 00-boilerplate/dist/account.styles.27e99e6a.js
@@ -184,7 +184,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel/src/builtins/bundle-url.js"}],"../node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../node_modules/parcel/src/builtins/bundle-url.js"}],"pages/account/account.styles.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./../../core/content/img/icon-user.svg":[["icon-user.4999b0f6.svg","core/content/img/icon-user.svg"],"core/content/img/icon-user.svg"],"_css_loader":"../node_modules/parcel/src/builtins/css-loader.js"}],"../node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
